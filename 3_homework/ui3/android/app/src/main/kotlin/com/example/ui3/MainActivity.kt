91251d2744d21eb8673311cd644f8e031b0729e4
Shadow-8888 <xushnudbekshodmonov888@gmail.com>
2025-06-30 11:53:27 +0500

    Delete 3_homework/ui3 directory

--- 3_homework/ui3/android/app/src/main/kotlin/com/example/ui3/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ui3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- 3_homework/ui3/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kWhiteColor=Color(0XFFFFFFFF);
-const Color kBlackColor=Color(0xFF000000);
-const Color kTextColor= Color(0xFF1D150B);
-const Color kPrimaryColor=Color(0xFFFB475F);
-const Color kSecondaryColor=Color(0xFFF5E1CB);
-const Color kBorderColor=Color(0xFFDDDDDD);
